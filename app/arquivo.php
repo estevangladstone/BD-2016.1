39700e3c572bcb4d7b60dde24bcd50c4b3bec69d
Estevan Gladstone <estevangladstone@ejcm.com.br>
2016-07-24 18:43:13 -0300

    Adicionando realização de queries pelo browser

--- app/arquivo.php
+++ app/arquivo.php
@@ -1,28 +1,32 @@
 <?php
 session_start();
 
-try {
-    $user = 'root';
-    $pass = '';
-    $dbh = new PDO('mysql:host=localhost;dbname=ivento3sudej2016;charset=utf8', $user, $pass);
-    
-    // $query = 'UPDATE criterios set total_vagas=10 WHERE id=4';
-    $query = $_GET["consulta"];
+if(!empty($_GET['consulta'])){
+    try {
+        $user = 'root';
+        $pass = '';
+        $dbh = new PDO('mysql:host=localhost;dbname=trabalhoBD;charset=utf8', $user, $pass);
+        
+        // $query = 'UPDATE criterios set total_vagas=10 WHERE id=4';
+        // $query = "select estados.nome as nomeEstado, municipios.nome as nomeMunicipio, estados.uf from municipios join estados on municipios.estado_id=estados.uf limit 10";
+        $query = $_GET["consulta"];
 
-    $consulta = $dbh->query($query);
+        $consulta = $dbh->query($query);
 
-    if ($consulta) {
-        if(strtolower(substr($query, 0, 6)) == 'select') {
-            $resultado = $consulta->fetchAll();
-            echo json_encode($resultado);
+        if ($consulta) {
+            if(strtolower(substr($query, 0, 6)) == 'select') {
+                $resultado = $consulta->fetchAll(PDO::FETCH_ASSOC);
+                $colunas = array_keys($resultado[0]);
+
+                $_SESSION['colunas'] = $colunas;
+                $_SESSION['resultados'] = $resultado;
+            }
         }
-    } else {
-        echo "consulta sem seleção";
-    }
 
-    $dbh = null; // fecha a conexão
-} catch (PDOException $e) {
-    print "Error!: " . $e->getMessage() . "<br/>";
-    die();
+        $dbh = null; // fecha a conexão
+    } catch (PDOException $e) {
+        print "Error!: " . $e->getMessage() . "<br/>";
+        die();
+    }
 }
 ?>
---
+++ index.php
@@ -0,0 +1,63 @@
+<?php 
+	require_once('app/arquivo.php');
+ ?>
+<html>
+<head>
+	<title>Site GEM 2016</title>
+	<meta charset="utf-8">
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.10.12/datatables.min.css"/>
+
+ </head>
+<body>
+	<div class="container">
+		<form id="form" action="index.php" method="get">
+			<div class="input-group">
+				<input type="text" id="consulta" class="form-control" name="consulta" placeholder="Digite sua consulta...">
+				<span class="input-group-btn">
+					<button id="teste" class="btn btn-default" type="submit">
+						<span class="glyphicon glyphicon-search" style="font-size: 20px"></span>
+					</button>
+				</span>
+			</div>
+		</form>
+		<table class="table" id="resultados">
+			<?php if(!empty($_SESSION['colunas']) && !empty($_SESSION['resultados'])) { ?>
+				<!-- Cabeçalho -->
+				<thead>
+					<?php foreach ($_SESSION['colunas'] as $column) { ?>
+						<?php if($column == 'id') { ?>
+							<th>#</th>
+						<?php } else { ?>
+							<th><?php echo $column ?></th>
+						<?php } ?>
+					<?php } unset($_SESSION['colunas']);//endforeach ?>
+				</thead>
+				<!-- Linhas -->
+				<tbody>
+					<?php foreach ($_SESSION['resultados'] as $linha) { ?>
+						<tr>
+							<?php foreach ($linha as $celula) { ?>
+								<td><?php echo $celula ?></td>
+							<?php } //endforeach ?>
+						</tr>
+					<?php } unset($_SESSION['resultados']); //endforeach ?>
+				</tbody>
+			<?php } else { ?>			
+				<tr>Nenhum resultado encontrado</tr>
+			<?php } //endif ?>
+		</table>
+	</div>
+
+	<script src="https://code.jquery.com/jquery-2.2.4.js" integrity="sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI=" crossorigin="anonymous"></script>
+	<script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.10.12/datatables.min.js"></script>
+	<script type="text/javascript">
+		$(document).ready(function() {
+		    $('#resultados').DataTable();
+		} );
+	</script>
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+</body>
+</html>
