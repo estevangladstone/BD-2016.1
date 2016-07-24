a04bca43b6e5a2773011b1db21a03e730cdee89f
Estevan Gladstone <estevangladstone@ejcm.com.br>
2016-07-24 00:43:59 -0300

    first commit

---
+++ app/arquivo.php
@@ -0,0 +1,23 @@
+<?php
+try {
+    $user = 'root';
+    $pass = '';
+    $dbh = new PDO('mysql:host=localhost;dbname=ivento3sudej2016;charset=utf8', $user, $pass);
+    
+    // $query = 'UPDATE criterios set total_vagas=10 WHERE id=4';
+    $query = 'SELECT * FROM criterios';
+
+    $consulta = $dbh->query($query);
+
+    if ($consulta) {
+        if(strtolower(substr($query, 0, 6)) == 'select')
+            $resultado = $consulta->fetchAll();
+        echo json_encode($resultado);
+    }
+
+    $dbh = null; // fecha a conexão
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
+?>
