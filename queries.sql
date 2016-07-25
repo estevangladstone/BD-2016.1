bc4cd84efa6b05330c269e4ac77379ff5ffd4358
Estevan Gladstone <estevangladstone@ejcm.com.br>
2016-07-24 21:09:22 -0300

    Adicionando mais queries ao arquivo

--- queries.sql
+++ queries.sql
@@ -3,6 +3,8 @@
 -- Envolve apenas seleção e projeção
 select id, nome, populacao, estado_id as uf from municipios where uf = 'RJ';
 
+
+
 -- Envolve junção de apenas duas relações
 select escola.nome, localizacao, rede, municipio.nome as municipio, municipio.estado_id as uf
 from escolas inner join municipios on escolas.municipio_id=municipio.id
@@ -14,11 +16,19 @@ select nome, rede, localizacao
 from escolas left join taxas on escolas.id=taxas.escola_id
 where rede = 'Federal' and  taxas.id = null;
 
+-- Envolve junção de apenas duas relações
+-- Obtém todos os telefones da escola de nome CEFET CELSO SUCKOW DA FONSECA
+select codigo, numero 
+from escolas inner join telefones on telefones.id=escolas.id
+where nome = 'CEFET CELSO SUCKOW DA FONSECA';
+
 -- Envolve junção externa de apenas duas relações
 -- Nomes, localização e taxas de todas as escolas da rede Federal e suas respectivas taxas de aprovação do ensino medio caso existam
 select nome, localizacao, (.colocar taxas aqui.)
 from escolas left join taxas on escolas.id=taxas.escola_id
-where rede = 'Federal' and ( (taxas.tipo = null or taxas.tipo = 'Aprovação') and (taxas.serie = null or taxas.serie like '%Médio'));
+where rede = 'Federal' and ( (taxas.tipo = null or taxas.tipo = 'Aprovação') and (taxas.serie = null or taxas.serie between 10 and 13));
+
+
 
 -- Envolve junção de três relações
 -- Obté o nome, rede, localização e uf de todas as escolas da região Sudeste 
@@ -26,17 +36,56 @@ select escolas.nome, rede, localizacao, uf
 from escolas inner join municipios on escolas.municipio_id=municipio.id inner join estados on estado.id=municipio.estado_id
 where estados.regiao = 'Sudeste';
 
--- Envolve junção de duas relações
--- Obtém todos os telefones da escola de nome CEFET CELSO SUCKOW DA FONSECA
-select codigo, numero 
-from escolas inner join telefones on telefones.id=escolas.id
-where nome = 'CEFET CELSO SUCKOW DA FONSECA';
+-- Envolve junção de três relações
+-- A media das taxas de aprovação do terceiro ano do ensino medio por estado
+select avg(taxas.valor) as media de aprovacao, uf, bandeira
+from escolas inner join taxas on escolas.id=taxas.escola_id inner join municipios on escolas.municipio_id=municipios.id inner join estados on estados.id=municipios.estado_id
+where taxas.serie = 12
+group by estado.uf;
 
+
+
+-- Consulta envolvendo função de agregação
 -- Obtém o número de escolas cadastradas no sistema por estado
-select count(distinct escolas.id), estados.nome, uf, regiao, (bandeira?)
+select count(distinct escolas.id), estados.nome, uf, regiao, bandeira
 from escolas inner join municipios on escolas.municipio_id=municipio.id inner join estados on estado.id=municipio.estado_id
+group by estados.uf
+
+-- Consulta envolvendo função de agregação
+-- Número de escolas cadastradas por região
+select count(distinct escolas.id), regiao
+from escolas inner join municipios on escolas.municipio_id=municipio.id inner join estados on estado.id=municipio.estado_id
+group by regiao;
+
+-- Consulta envolvendo função de agregação
+-- Número de escolas cadastradas que não preencheram nenhum dado por região
+select count(distinct escolas.id), regiao
+from escolas inner join municipios on escolas.municipio_id=municipio.id inner join estados on estado.id=municipio.estado_id left join taxas on escolas.id=taxas.escola_id
+where taxas.id = null
+group by regiao;
+
+
+-- Consulta envovendo subconsultas aninhadas
+
+-- Consulta envovendo subconsultas aninhadas
+
+
+-- Consulta do tipo relatório
+-- A escola com maior taxa de abandono do terceiro ano do ensino medio na cidade do rio de janeiro agrupadas por rede
+select max(taxas.valor), escolas.nome, rede, localizacao
+from escolas inner join taxas on escolas.id=taxas.escola_id
+where taxas.serie = 12 and taxas.tipo = 'Abandono'
+group by escolas.rede;
+
+-- Consulta do tipo relatório
+-- A escola com maior taxa de abandono do terceiro ano do ensino medio na cidade do rio de janeiro agrupadas por rede
+select min(taxas.valor), escolas.nome, rede, localizacao
+from escolas inner join taxas on escolas.id=taxas.escola_id
+where taxas.serie = 12 and taxas.tipo = 'Abandono'
+group by escolas.rede;
 
 -- -- Obtém o número de escolas cadastradas no sistema e o numero de escolas que possuem taxas preenchidas por estado
 -- select count(distinct escolas.id), estados.nome, uf, regiao, (bandeira?)
 -- from escolas inner join municipios on escolas.municipio_id=municipio.id inner join estados on estado.id=municipio.estado_id inner join taxas on escolas.id=taxas.escola_id
 -- where sasdasddsads
+
